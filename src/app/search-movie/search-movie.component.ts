95495bd44f493d560fbf747cd4647a46748a87c2
alexey-kozlenkov <akozlenkov@exadel.com>
2018-01-23 22:52:37 +0200

    Add search movie as an example of template ref variable

---
+++ src/app/search-movie/search-movie.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'movie-search',
+  templateUrl: 'search-movie.component.html'
+})
+export class MovieSearchComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() { }
+
+}
