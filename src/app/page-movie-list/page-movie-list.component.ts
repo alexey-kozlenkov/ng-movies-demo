6177d9d6f08ab46a2f990e665b1e331567a7b1de
alexey-kozlenkov <akozlenkov@exadel.com>
2018-01-24 11:16:58 +0200

    Implement movie-list component

--- src/app/page-movie-list/page-movie-list.component.ts
+++ src/app/page-movie-list/page-movie-list.component.ts
@@ -1,10 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { Movie } from '../types/movie';
 
 @Component({
   selector: 'movie-list',
   templateUrl: 'page-movie-list.component.html'
 })
 export class PageMovieListComponent implements OnInit {
+
+  movies: Movie[] = [
+    {
+      id: 0,
+      title: 'Dark Knight',
+      originalTitle: 'Dark Knight',
+      overview: 'Christopher Nolan is a genius',
+      releaseDate: '2009-01-01',
+      originalLanguage: 'en',
+      voteAverage: 5,
+    },
+  ];
+
   constructor() { }
 
   ngOnInit() { }
