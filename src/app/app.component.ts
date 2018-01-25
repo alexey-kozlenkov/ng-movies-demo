cd84773d4ba1a2ae1867bdb935503ba91e014c40
alexey-kozlenkov <akozlenkov@exadel.com>
2018-01-25 10:50:18 +0200

    Remove movies spec

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,11 @@
 import { Component } from '@angular/core';
 
-const MOVIES_SPEC = [
-  {
-    title: 'Godfather',
-    director: 'Francis Ford Coppola',
-    releaseDate: '1970-01-01',
-
-    isFavorite: false,
-    countryCode: 'it',
-  },
-  {
-    title: 'Dark Knight',
-    director: 'Christopher Nolan',
-    releaseDate: '2009-01-01',
-
-    isFavorite: true,
-    countryCode: 'gt',
-  },
-  {
-    title: 'Deadpool',
-    director: 'Guy Richy',
-    releaseDate: '2015-02-12',
-
-    isFavorite: true,
-    countryCode: 'uk',
-  },
-  {
-    title: 'Pulp Fiction',
-    director: 'Quentin Tarantino',
-    releaseDate: '1994-01-01',
-
-    isFavorite: false,
-    countryCode: 'us',
-  },
-  {
-    title: 'Redirected',
-    director: 'Emilis Velivis',
-    releaseDate: '2013-01-01',
-
-    isFavorite: true,
-    countryCode: 'lt',
-  },
-];
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  movies = MOVIES_SPEC;
-
   onToggleFavorite(isFavorite: boolean, movie) {
     movie.isFavorite = isFavorite;
   }
